9516fdc7b3086a5c64b12844abc422362bc10ba1
Alex <t9371100211@gmail.com>
2021-06-10 20:34:01 +0300

    Commit before travel

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -4,7 +4,22 @@ const SEND_MESSAGE = "SEND-MESSAGE",
 export const sendMessageCreator = () => ({type: SEND_MESSAGE});
 export const updateMessageCreator = (value) => ({type: UPDATE_NEW_MESSAGE, value: value});
 
-const dialogsReducer = (state, action) => {
+const initState = {
+    messagesPage: {
+        dialogData: [
+            {id: 1, name: "Alex"},
+            {id: 2, name: "Sergey"},
+            {id: 3, name: "Artem"}
+        ],
+        messageData: [
+            {id: 1, message: "Hi man"},
+            {id: 2, message: "How you doing?"},
+        ],
+        newMessage: "",
+    }
+}
+
+const dialogsReducer = (state=initState, action) => {
     switch (action.type) {
         case SEND_MESSAGE:
             if (state.newMessage) {
